3353d247a937617a3a5d2519908f9f7dcdd414db
Rodolpho Rangel de Barros <mr_condor_@hotmail.com>
2020-11-23 15:12:30 -0300

    Add project files.

---
+++ src/Acorda.API/Controllers/AcordaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Acorda.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AcordaController : Controller
+    {
+        [HttpPost("Acorda/{macAddress}")]
+        public async Task<IActionResult> Acorda(string macAddress)
+        {
+            try
+            {
+                await WOL.WakeOnLan(macAddress);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
